3682f4caf8fc36c7e646c38e1c3b2c3feb7d3178
Arif <kanebrian0634@gmail.com>
2022-07-21 16:53:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AuthContextProvider from './context/AuthContext';
+import AppRouter from './router/AppRouter';
+import { ToastContainer  } from 'react-toastify';
+
+const App = () => {
+  return (
+    <div>
+      <AuthContextProvider>
+        <AppRouter />
+        <ToastContainer />
+      </AuthContextProvider>
+    </div>
+  );
+};
+
+export default App;
